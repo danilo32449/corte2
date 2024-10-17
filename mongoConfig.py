f9bbcc266b32eeb622e9a5dd53e1add07abf0d87
danilo32449 <127262132+danilo32449@users.noreply.github.com>
2024-10-16 20:14:41 -0500

    Add files via upload

---
+++ mongoConfig.py
@@ -0,0 +1,6 @@
+config = {
+    "URI":"mongodb+srv://lopezdanielfelipe454:BaimoTZfUtj8dQdi@cluster0.2iivi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "DATABASE":"Maquina",
+    "COLLECTION":"tabla"
+}
+
